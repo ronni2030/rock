5f6e0c2dcabd452c8b1dbabaf463a07c3be74f7a
ronni2030 <rav.villa@yavirac.edu.ec>
2026-06-02 16:42:54 -0500

    app

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import Ozzy from './pages/ozzy/OZZY';
 import OzzyHistoria from './pages/ozzy/OzzyHistoria';
 import OzzyAlbunes from './pages/ozzy/OzzyAlbunes';
 import OzzyGrupo from './pages/ozzy/OzzyGrupo';
-import Kiss from './pages/kiss/Kiss';
-import KissHistoria from './pages/kiss/KissHistoria';
-import KissAlbunes from './pages/kiss/KissAlbunes';
-import KissGrupo from './pages/kiss/KissGrupo';
-import IronMaiden from './pages/ironmaiden/IronMaiden';
-import IronMaidenHistoria from './pages/ironmaiden/IronMaidenHistoria';
-import IronMaidenAlbunes from './pages/ironmaiden/IronMaidenAlbunes';
-import IronMaidenGrupo from './pages/ironmaiden/IronMaidenGrupo';
-import BlackSabbath from './pages/blacksabbath/BlackSabbath';
-import BlackSabbathHistoria from './pages/blacksabbath/BlackSabbathHistoria';
-import BlackSabbathAlbunes from './pages/blacksabbath/BlackSabbathAlbunes';
-import BlackSabbathGrupo from './pages/blacksabbath/BlackSabbathGrupo';
+import Kiss from './pages/Kiss/Kiss';
+import KissHistoria from './pages/Kiss/KissHistoria';
+import KissAlbunes from './pages/Kiss/KissAlbunes';
+import KissGrupo from './pages/Kiss/KissGrupo';
+import IronMaiden from './pages/Iron Maiden/IronMaiden';
+import IronMaidenHistoria from './pages/Iron Maiden/IronMaidenHistoria';
+import IronMaidenAlbunes from './pages/Iron Maiden/IronMaidenAlbunes';
+import IronMaidenGrupo from './pages/Iron Maiden/IronMaidenGrupo';
+import BlackSabbath from './pages/Black Sabbath/BlackSabbath';
+import BlackSabbathHistoria from './pages/Black Sabbath/BlackSabbathHistoria';
+import BlackSabbathAlbunes from './pages/Black Sabbath/BlackSabbathAlbunes';
+import BlackSabbathGrupo from './pages/Black Sabbath/BlackSabbathGrupo';
 
 const App = () => {
     return (
